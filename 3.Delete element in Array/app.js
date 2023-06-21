47ec557ddb659ca2d739f0228f25a84411d89ddc
28vikas <63vikas@gmail.com>
2023-06-21 22:01:27 +0530

    delete element in array

---
+++ 3.Delete element in Array/app.js
@@ -0,0 +1,26 @@
+//-------------without in built function --------------//
+
+// let arr = [20,50,80,90,70,50,10]
+
+// let position = 3;
+
+// for (let i = position; i<arr.length-1; i++){
+//     arr[i] = arr[i+1];
+// }
+// arr.length = arr.length-1;
+
+// console.log(arr)
+
+
+
+
+//---------------in built function-------------//
+
+// let arr = [20,50,80,90,70,50,10]
+
+// arr.splice(3,1,)
+
+// console.log(arr)
+
+
+
