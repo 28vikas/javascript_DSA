8345c5c3a0cf25253a084cfab8325d4049f33130
28vikas <63vikas@gmail.com>
2023-06-22 20:25:59 +0530

    search element in array

---
+++ 4.Search element in  array/app.js
@@ -0,0 +1,27 @@
+//------------find element in array-----without in built function------------//
+
+// let arr = [10,50,20,80,70,60]
+
+// let elem = 20;
+// let index = undefined;
+
+// for(let i = 0; i<arr.length; i++){
+     
+// if(arr[i] === elem){
+//    index = i;
+//    break;
+// }
+
+// }
+// console.log(index)
+
+
+
+//------------find element in array-----in built function------------//
+
+
+// let arr = [10,50,20,80,70,60]
+
+// let elem = 20;
+
+// console.log(arr.indexOf(elem))
